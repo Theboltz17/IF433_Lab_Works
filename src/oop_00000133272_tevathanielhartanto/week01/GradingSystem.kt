240a5ab2ad0e6a5cdbac6bf2f2a944d7c488ffe7
Theboltz17 <tevathanielhartanto@gmail.com>
2026-02-03 17:18:29 +0700

    week01: initial setup and procedural code

---
+++ src/oop_00000133272_tevathanielhartanto/week01/GradingSystem.kt
@@ -0,0 +1,9 @@
+package oop_00000133272_tevathanielhartanto.week01
+
+fun main() {
+    var name: String = "John Thor"
+    var score: Int = 80
+
+    // Concatenation gaya lama
+    println("Nama: " + name + ", Nilai: " + score)
+}
