7f93a6bce65a01d37a4aa4012d696f874f23e794
Theboltz17 <tevathanielhartanto@gmail.com>
2026-02-03 19:44:03 +0700

    week01: (tugas 2) implement receipt printing

--- src/oop_00000133272_tevathanielhartanto/week01/GameStore.kt
+++ src/oop_00000133272_tevathanielhartanto/week01/GameStore.kt
@@ -16,7 +16,7 @@ fun calculateDiscount(price: Int) =
     if (price > 500_000) price * 80 / 100
     else price * 90 / 100
 
-fun printReceipt(title: String, finalPrice: int) {
+fun printReceipt(title: String, finalPrice: Int) {
     println("Judul game : $title")
     println("Harga akhir: Rp$finalPrice")
 }
