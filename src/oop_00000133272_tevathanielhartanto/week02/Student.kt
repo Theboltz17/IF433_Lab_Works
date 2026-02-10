b891963c3cae55e8afc82b6953852f79e30dd194
Theboltz17 <tevathanielhartanto@gmail.com>
2026-02-10 16:45:49 +0700

    week02: challenge refactor primary constructor using default args

--- src/oop_00000133272_tevathanielhartanto/week02/Student.kt
+++ src/oop_00000133272_tevathanielhartanto/week02/Student.kt
@@ -3,7 +3,8 @@ package oop_00000133272_tevathanielhartanto.week02
 class Student (
     val name: String,
     val nim: String,
-    var major: String
+    var major: String,
+    var gpa: Double = 0.0, // <-- Default Argument
 ){
     init {
         // Validasi Sederhana: Cek panjang NIM
@@ -16,7 +17,7 @@ class Student (
     }
     // Secondary Constructor
     // Wajib memanggil Primary menggunakan 'this()'
-    constructor(name: String, nim: String) : this(name, nim, "Non-Matriculated") {
+    constructor(name: String, nim: String) : this(name, nim, major = "Non-Matriculated") {
         println("LOG: Menggunakan constructor jalur umum (Tanpa Jurusan). ")
     }
 }
