19ca2a492a6220f4c304003df3217bc86995f286
Theboltz17 <tevathanielhartanto@gmail.com>
2026-02-21 19:26:15 +0700

    week03: (task 1) create weapon class with validation

---
+++ src/oop_00000133272_tevathanielhartanto/week03/Weapon.kt
@@ -0,0 +1,27 @@
+package oop_00000133272_tevathanielhartanto.week03
+
+class Weapon(val name: String) {
+
+    var damage: Int = 0
+        set(value) {
+            when {
+                value < 0 -> {
+                    println("Damage tidak boleh negatif!")
+                }
+                value > 1000 -> {
+                    println("Damage terlalu besar! Diset ke 1000.")
+                    field = 1000
+                }
+                else -> {
+                    field = value
+                }
+            }
+        }
+
+    val tier: String
+        get() = when {
+            damage > 800 -> "Legendary"
+            damage > 500 -> "Epic"
+            else -> "Common"
+        }
+}
