896024268e8d4414be76e4b8fd1825da09a890c3
Theboltz17 <tevathanielhartanto@gmail.com>
2026-03-10 19:12:43 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_00000133272_tevathanielhartanto/week06/GadgetInterfaces.kt
@@ -0,0 +1,9 @@
+package oop_00000133272_tevathanielhartanto.week06
+
+interface Camera {
+    fun turnOn() { println("Lensa kamera terbuka dan sensor aktif.") }
+}
+
+interface Phone {
+    fun turnOn() { println("Sinyal seluler mencari jaringan.") }
+}
