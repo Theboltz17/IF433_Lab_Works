25dbd98710c092edec45437f97a7e178792d56e6
Theboltz17 <tevathanielhartanto@gmail.com>
2026-03-17 15:46:50 +0700

    week07: (task) create ItemRarity enum

---
+++ src/oop_00000133272_tevathanielhartanto/week07/GameModels.kt
@@ -0,0 +1,9 @@
+package oop_00000133272_tevathanielhartanto.week07
+
+enum class ItemRarity(val dropChance: Int) {
+    COMMON(70),
+    UNCOMMON(40),
+    RARE(20),
+    EPIC(5),
+    LEGENDARY(1)
+}
