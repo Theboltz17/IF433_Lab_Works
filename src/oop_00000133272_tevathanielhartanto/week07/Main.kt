cc86290c9cd1fbce62998d2673534ffd0d25cab0
Theboltz17 <tevathanielhartanto@gmail.com>
2026-03-17 15:06:30 +0700

    week07: (trial) trigger private constructor error on NetworkClient

---
+++ src/oop_00000133272_tevathanielhartanto/week07/Main.kt
@@ -0,0 +1,3 @@
+package oop_00000133272_tevathanielhartanto.week07
+
+val client = NetworkClient("https://api.umn.ac.id")
---
+++ src/oop_00000133272_tevathanielhartanto/week07/NetworkClient.kt
@@ -0,0 +1,7 @@
+package oop_00000133272_tevathanielhartanto.week07
+
+class NetworkClient private constructor(val urL: String) {
+    fun connect() {
+        println("Connectiong to $url...")
+    }
+}
