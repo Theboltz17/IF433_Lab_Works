8971241e06d27a1a1a7c99e6b9fa060e0e5e8c00
Theboltz17 <tevathanielhartanto@gmail.com>
2026-04-14 15:31:03 +0700

    week08: (task) create sealed class Product and data classes

---
+++ src/oop_00000133272_tevathanielhartanto/week08/EcommerceModels.kt
@@ -0,0 +1,15 @@
+package oop_00000133272_tevathanielhartanto.week08
+
+sealed class Product()
+
+data class Electronic(
+    val id: String,
+    val name: String,
+    val warrantyMonths: Int
+) : Product()
+
+data class Clothing(
+    val id: String,
+    val name: String,
+    val size: String
+) : Product()
