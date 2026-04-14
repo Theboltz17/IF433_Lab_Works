a8ad8689d0f0f9923b81df84a2c024cb98462d47
Theboltz17 <tevathanielhartanto@gmail.com>
2026-04-14 15:36:31 +0700

    week08: (task) implement payment processing with !!

--- src/oop_00000133272_tevathanielhartanto/week08/ApiParser.kt
+++ src/oop_00000133272_tevathanielhartanto/week08/ApiParser.kt
@@ -17,13 +17,21 @@ class ApiParser {
                 val warranty = rawJson["warranty"] as? Int ?: 12
                 Electronic(id = id, name = name, warrantyMonths = warranty)
             }
-
             "CLOTHING" -> {
                 val size = rawJson["size"] as? String ?: "All Size"
                 Clothing(id = id, name = name, size = size)
             }
-
             else -> null
         }
+    }
+
+    fun checkout(product: Product) {
+        val productId = when (product) {
+            is Electronic -> product.id
+            is Clothing   -> product.id
+        }
+
+        val transactionId = JavaPaymentService.processPayment(productId)!!
+        println("✅ Checkout sukses! Transaction ID: $transactionId")
     }
 }
