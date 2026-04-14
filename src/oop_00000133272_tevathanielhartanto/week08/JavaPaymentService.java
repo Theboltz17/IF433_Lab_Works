592369ddbf761536278b24143c7b58a35bedd0e7
Theboltz17 <tevathanielhartanto@gmail.com>
2026-04-14 15:34:18 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_00000133272_tevathanielhartanto/week08/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_00000133272_tevathanielhartanto.week08;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
