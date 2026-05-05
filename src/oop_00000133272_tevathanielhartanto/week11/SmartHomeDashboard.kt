3b9ccbe7862573455c1a6eabb7d6ecb565df5b65
Theboltz17 <tevathanielhartanto@gmail.com>
2026-05-05 14:46:53 +0700

    week11: (task) configure Philips WiZ using apply

--- src/oop_00000133272_tevathanielhartanto/week11/SmartHomeDashboard.kt
+++ src/oop_00000133272_tevathanielhartanto/week11/SmartHomeDashboard.kt
@@ -2,4 +2,11 @@ package oop_00000133272_tevathanielhartanto.week11
 
 fun main() {
     val homeDevices = mutableListOf<SmartDevice>()
+
+    SmartDevice("Philips WiZ Living Room", "Lighting").apply {
+        isOnline = true
+        powerLoad = 12
+    }.also {
+        homeDevices.add(it)
+    }
 }
