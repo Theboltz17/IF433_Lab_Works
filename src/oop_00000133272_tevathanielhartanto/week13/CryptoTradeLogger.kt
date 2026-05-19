d051851bff94ed432d268c34a7fa1141e45693b4
Theboltz17 <tevathanielhartanto@gmail.com>
2026-05-19 14:22:20 +0700

    week13: (task) inject malformed line via appendText to test robustness

--- src/oop_00000133272_tevathanielhartanto/week13/CryptoTradeLogger.kt
+++ src/oop_00000133272_tevathanielhartanto/week13/CryptoTradeLogger.kt
@@ -52,4 +52,7 @@ fun main() {
     )
     saveTrades(trades, "crypto_trades.csv")
     println("Data trade berhasil disimpan.")
+
+    File("crypto_trades.csv").appendText("CORRUPT_ID,DOGEUSDT,Hold,XX,YY\n")
+    println("Baris korup berhasil di-inject untuk uji robustness.")
 }
